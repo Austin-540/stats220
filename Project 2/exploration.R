f64419eb2125cb98e7a3e2c4392717e44db8ff75
Austin <105258719+Austin-540@users.noreply.github.com>
2026-03-30 18:51:53 +1300

    Update exploration.R

--- Project 2/exploration.R
+++ Project 2/exploration.R
@@ -2,7 +2,6 @@ library(tidyverse)
 
 logged_data <- read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vRetKKj9bMRzvsbYOusSWE0uw2oEJIxmDjFP6C2U79SCUlN05jlOH4OHHo5kVT4mpo5BdYi9q9NZh7w/pub?gid=49993136&single=true&output=csv")
 
-
 latest_data <- rename(logged_data, timestamp = 1, method = 2, duration = 3, start_time = 4)
 
 #View(latest_data)
@@ -45,6 +44,12 @@ ggplot(latest_data.rounded) +
 # For my report I will use the rounded data chart and number of journeys by method plots
 # And I will include the number of rows of data collected, and the mean journey time
 # This will require the following R code:
+
+logged_data <- read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vRetKKj9bMRzvsbYOusSWE0uw2oEJIxmDjFP6C2U79SCUlN05jlOH4OHHo5kVT4mpo5BdYi9q9NZh7w/pub?gid=49993136&single=true&output=csv")
+
+latest_data <- rename(logged_data, timestamp = 1, method = 2, duration = 3, start_time = 4)
+
+
 
 print(str_glue("There have been {nrow(latest_data)} journeys tracked by this form.")) #Useful
 print(str_glue("Mean: {mean(latest_data$duration)}")) #The mean is the most useful here

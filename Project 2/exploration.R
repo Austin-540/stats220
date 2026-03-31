f946ff7a6a5e772f7327fb44f50509c1be0ae27c
Austin <105258719+Austin-540@users.noreply.github.com>
2026-03-31 20:45:24 +1300

    Update a whole bunch of stuff

--- Project 2/exploration.R
+++ Project 2/exploration.R
@@ -1,8 +1,16 @@
+#
+# I have split this document into sections so that I'm not repeating a bunch of code and making
+# things confusing with my comments at the bottom. The section number is in the comment *after* the code.
+#
+
 library(tidyverse)
 
 logged_data <- read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vRetKKj9bMRzvsbYOusSWE0uw2oEJIxmDjFP6C2U79SCUlN05jlOH4OHHo5kVT4mpo5BdYi9q9NZh7w/pub?gid=49993136&single=true&output=csv")
 
 latest_data <- rename(logged_data, timestamp = 1, method = 2, duration = 3, start_time = 4)
+# ^^^^^ section 1
+
+
 
 #View(latest_data)
 
@@ -11,6 +19,8 @@ print(str_glue("Min: {min(latest_data$duration)}, Max: {max(latest_data$duration
 
 ggplot(latest_data) + geom_bar(aes(x=duration))
 #Not very useful
+
+# ^^^^^ section 2
 
 latest_data_durations.rounded <- latest_data$duration %>% 
   round(-1) #Round the durations to the 10s place
@@ -26,6 +36,8 @@ ggplot(latest_data.rounded) +
        )
 #Thats a lot more useful than before
 
+#^^^^^^ section 3
+
 ggplot(latest_data.rounded) + 
   geom_bar(aes(x=method), fill="#000080") +
   theme_minimal() +
@@ -35,6 +47,8 @@ ggplot(latest_data.rounded) +
 #Kind of interesting to see the ratio, but its not that useful.
 
 
+#^^^^^^^ section 4
+
 latest_data.bus <- latest_data
 latest_data.car <- latest_data
 for (i in nrow(latest_data):1) { #I feel like there might be a better way to do this in module 3...
@@ -43,18 +57,19 @@ for (i in nrow(latest_data):1) { #I feel like there might be a better way to do 
   } else if (latest_data$method[i] == "Car") {
     latest_data.bus <- latest_data.bus[-i,]
  } 
-}
+} #Starting with the final element, go through each element and remove it from the different dfs if it isn't the corresponding method
 
 mean_times <- data.frame(method=c("Bus", "Car"), duration=c(mean(latest_data.bus$duration), mean(latest_data.car$duration)))
 ggplot(mean_times) + 
-  geom_col(aes(x=method, y=duration), fill="#36fd45") + #Had to switch the type of geom here
+  geom_col(aes(x=method, y=duration), fill="#36fd45") + #Had to switch the type of geom here - not sure why but the ggplot docs say to
   theme_minimal()
 #Honestly right now it doesn't show a big difference but maybe in the future it will. Either way it's useful.
 
+#^^^^^^ section 5
 
 
 
-# For my report I will use the rounded data chart and mean duration by method plots
+# For my report I will use the rounded data chart and mean duration by method chart
 # And I will include the number of rows of data collected, and the mean journey time
 # This will require the following R code:
 
@@ -62,41 +77,6 @@ logged_data <- read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vRetKKj
 
 latest_data <- rename(logged_data, timestamp = 1, method = 2, duration = 3, start_time = 4)
 
-
-
-print(str_glue("There have been {nrow(latest_data)} journeys tracked by this form.")) #Useful
-print(str_glue("Mean: {mean(latest_data$duration)}")) #The mean is the most useful here
-
-
-
-latest_data_durations.rounded <- latest_data$duration %>% 
-  round(-1) #Round the durations to the 10s place
-latest_data.rounded <- latest_data
-latest_data.rounded$duration <- latest_data_durations.rounded
-
-ggplot(latest_data.rounded) + 
-  geom_bar(aes(x=duration, fill = method)) + 
-  theme_minimal() +
-  labs(x = "Duration in minutes - rounded to the closest 10 minutes",
-       y = "Number of journies",
-       title = "Durations of people in the data's commutes to and from campus.",
-  )
-
-
-latest_data.bus <- latest_data
-latest_data.car <- latest_data
-for (i in nrow(latest_data):1) { #I'm sure we will cover a much better way of doing this in a future module...
-  # For each row in the data frames, starting with the final ones
-  if (latest_data$method[i] == "Bus") { #If its a bus journey, remove that row from the car df
-    latest_data.car <- latest_data.car[-i,]
-  } else if (latest_data$method[i] == "Car") { #If its a car journey, remove that row from the bus df
-    latest_data.bus <- latest_data.bus[-i,]
-  } 
-}
-
-mean_times <- data.frame(method=c("Bus", "Car"), duration=c(mean(latest_data.bus$duration), mean(latest_data.car$duration)))
-ggplot(mean_times) + 
-  geom_col(aes(x=method, y=duration), fill="#26ed35") + #Had to switch the type of geom here
-  theme_minimal() +
-  labs(x = "Transportation method", y = "Mean duration in minutes",
-       title = "Mean commute duration by method")
+print(str_glue("There have been {nrow(latest_data)} journeys tracked by this form.")) 
+print(str_glue("Mean: {mean(latest_data$duration)}"))
+# and sections 3 & 5

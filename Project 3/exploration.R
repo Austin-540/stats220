dc4b38828cc8cbf192835d6849a98e1f864116db
Austin <105258719+Austin-540@users.noreply.github.com>
2026-04-26 16:32:18 +1200

    Almost done report

--- Project 3/exploration.R
+++ Project 3/exploration.R
@@ -21,7 +21,7 @@ photo_data <- tibble(photos = data$photos) %>%
 selected_photos <- photo_data %>%
   filter(str_to_lower(substr(alt, 1, 1)) == "a") %>%
   #Filter it down to only photos where the alt text either starts with "A" or "a".
-  #This is personally relevant to me as A is the first letter of my name.
+  #This is personally relevant to me because A is the first letter of my name.
   mutate(
     is_portrait = width < height, #If the photo is a square then it isn't portrait, so this will be false
     words_in_photographers_username = str_count(photographer, "\\S+"), 
@@ -66,4 +66,52 @@ selected_photos <- photo_data %>%
   )
 
 write_csv(selected_photos, "selected_photos.csv")
+
+
+
+# part D
+
+percent_blue <- mean(selected_photos$main_colour == "blue") %>%
+  round(digits=3) * 100
+print(paste0(percent_blue, "% of the selected photos have more blue than red or green"))
+
+
+
+proportion_blue_portrait <- selected_photos %>% 
+  group_by(is_portrait) %>%
+  summarise(proportion_blue = mean(main_colour == "blue"))
+
+proportion_blue_not_portrait <- proportion_blue_portrait$proportion_blue[1]
+
+
+
+mean_num_author_words_by_is_portrait <- selected_photos %>%
+  group_by(is_portrait) %>%
+  summarise(mean_words_in_author = mean(words_in_photographers_username))
+difference <- round(mean_num_author_words_by_is_portrait$mean_words_in_author[1] - mean_num_author_words_by_is_portrait$mean_words_in_author[2], digits=3)
+
+# part E
+
+
+images <- NULL
+for (i in 1:nrow(selected_photos)) {
+  current_image <- image_read(selected_photos$small[i]) %>%
+    image_scale("300") %>%
+    image_annotate(
+      text = ifelse(selected_photos$main_colour[i] == "blue",
+                    "BLUE PICTURE",
+                    "NOT MOSTLY\nBLUE!!"),
+      size = 35,
+      boxcolor = "#FFFFFF",
+      gravity = "north"
+    )
+  if (is.null(images)) {
+    images <- c(current_image)
+  } else {
+    images <- c(images, current_image)
+  }
+}
+
+image_animate(images, fps=1) %>%
+  image_write("creativity.gif") 
 

6c8e46a9333b65e56d2edff3631cec1b80ac37fe
Austin <105258719+Austin-540@users.noreply.github.com>
2026-05-13 17:12:11 +1200

    Update plot sizes

--- Project 4/data_visualisation.R
+++ Project 4/data_visualisation.R
@@ -28,7 +28,7 @@ ggplot(logged_data) +
     y = "Number of commutes",
     fill = "Who commuted?"
   )
-  ggsave("plot1.png")
+ggsave("plot1.png", width = 6, height = 4, unit= "in")
 
 
 
@@ -79,7 +79,7 @@ logged_data %>%
       "Car" = "Armand"
     )
   ) 
-ggsave("plot2.png")
+ggsave("plot2.png", width = 6, height = 4, unit= "in")
 
 
 
@@ -126,7 +126,7 @@ logged_data %>%
     x = "Where is the data from?",
     y = "Median commute duration"
   ) 
-ggsave("plot3.png")
+ggsave("plot3.png", width = 6, height = 4, unit= "in") # inches 🤮
     
   
 
